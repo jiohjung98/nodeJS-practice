b43efef398c9887bf41b48c01234706904c9cd7f
jiohjung98 <ckaquz98@naver.com>
2024-02-07 18:28:45 +0900

    practice: api server

--- index.js
+++ index.js
@@ -13,7 +13,21 @@ app.get('/dog', (req, res) => {
     res.json({'sound': '멍멍'});
 });
 
-app.get('/cat', (req, res) => {
+app.get('/sound/:name', (req, res) => {
+    const { name } = req.params;
+
+    if (name == 'dog') {
+        res.json({'sound': '멍멍'});
+    } else if (name == 'cat') {
+        res.json({'sound': '냐옹'});
+    } else if (name == 'pig') {
+        res.json({'sound': '꿀꿀'});
+    } else {
+        res.json({'sound': '알 수 없음'});
+    };
+
+    console.log(name);
+
     res.json({'sound': '냐옹'});
 });
 
@@ -32,7 +46,7 @@ app.get('/user/:id', (req, res) => {
 });
 
 
-// query - /users/원하는아이디/?q= ~ 
+// query - /users/원하는아이디/?q= ~ (key, value 구조)
 
 app.get('/users/:id', (req, res) => {
     const q = req.query;
